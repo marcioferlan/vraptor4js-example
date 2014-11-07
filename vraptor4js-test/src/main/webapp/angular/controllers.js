41e161c02c25a0618f9291bb8904a3611f1428c3
Marcio Lima <marcio@gtisolucoes.com>
2014-11-07 04:08:44 -0200

    Example of success and error callbacks

--- vraptor4js-test/src/main/webapp/angular/controllers.js
+++ vraptor4js-test/src/main/webapp/angular/controllers.js
@@ -1,8 +1,11 @@
 angular.module('app.controllers', ['app.services'])
 	.controller('TestCtrl', function($scope, IndexController) {
 		// This is your JS controller
-		IndexController.index(function(resp){
+		IndexController.index(function(resp) { // success callback
 			$scope.serverMessage = resp.string;
+		},
+		function(error, status) { // error callback
+			console.log(error);			
 		});
 	});
 
