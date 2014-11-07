4d96eb73aff18239b2cac5d486fa2d5c9494edc1
Marcio Lima <marcio@gtisolucoes.com>
2014-11-07 04:09:11 -0200

    Annotating controller with @V4js

--- vraptor4js-api/src/main/java/br/com/caelum/vraptor/controller/IndexController.java
+++ vraptor4js-api/src/main/java/br/com/caelum/vraptor/controller/IndexController.java
@@ -7,6 +7,9 @@ import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.view.Results;
 
+import com.github.vraptor4js.V4js;
+
+@V4js
 @Controller
 public class IndexController {
 
@@ -17,4 +20,5 @@ public class IndexController {
 	public void index() {
 		result.use(Results.json()).from("It works! You have installed VRaptor4js plugin correctly.").serialize();
 	}
+
 }
